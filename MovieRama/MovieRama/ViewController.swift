bc7930f76ad12f24803c3522c5daca98daa7fc43
Panagiotis Mourgias <pmourgias@gmail.com>
2022-06-22 15:46:49 +0300

    setup project

---
+++ MovieRama/MovieRama/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright © 2022 Panagiotis Mourgias
+//  All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
