bacda8553436342271a11d290e36c306fe9690b4
Panagiotis Mourgias <pmourgias@gmail.com>
2022-06-23 15:03:26 +0300

    improve Popular ViewModel - ViewController

--- MovieRama/MovieRama/UI/PopularMovies/PopularMoviesViewController.swift
+++ MovieRama/MovieRama/UI/PopularMovies/PopularMoviesViewController.swift
@@ -10,10 +10,38 @@ class PopularMoviesViewController: BaseViewController {
     
     // MARK: Properties
     
-    var viewModel: PopularMoviesViewModelProtocol = PopularMoviesViewModel()
+    private var cancellable = Cancellable()
+    
+    var viewModel = PopularMoviesViewModel()
+    
+    // MARK: UI Properties
+    
+    private lazy var tableView: BaseTableView = {
+        let tableView = BaseTableView(frame: .zero, style: .plain)
+        
+        tableView.separatorColor = .clear
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        tableView.allowsSelection = true
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.showsVerticalScrollIndicator = true
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        tableView.register(MovieTableViewCell.self,
+                           forCellReuseIdentifier: MovieTableViewCell.id)
+        
+        tableView.showRefreshControl = true
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel.fetchPopular()
         
         setupView()
         bindViewModel()
@@ -23,11 +51,84 @@ class PopularMoviesViewController: BaseViewController {
     
     private func setupView() {
         
+        view.addSubview(tableView)
+        
+        tableView.layout(
+            .top(0),
+            .leading(0),
+            .trailing(0),
+            .bottom(0)
+        )
+        
+        tableView.addRefreshTarget(self, action: #selector(refreshContent))
     }
     
     // MARK: Bind View Model
     
     private func bindViewModel() {
-        viewModel.fetchPopular()
+        
+        viewModel.reloadData.done { [weak self] in
+            guard let self = self else { return }
+            
+            self.tableView.endRefreshingControl()
+            self.tableView.reloadData()
+        }
+        .store(in: &cancellable)
+    }
+    
+    @objc
+    func refreshContent() {
+        self.tableView.beginRefreshingControl()
+        
+        // Perform some delay
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) { [weak self] in
+            guard let self = self else { return }
+            self.viewModel.refreshContent()
+        }
+    }
+}
+
+// MARK: UITableViewDelegate - UITableViewDataSource
+
+extension PopularMoviesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows()
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        let lastSectionIndex = tableView.numberOfSections - 1
+        let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
+        
+        if indexPath.section == lastSectionIndex && indexPath.row == lastRowIndex {
+            // print("will request new data 🔃")
+            
+            // Unwrap first, then avoid next api call if reach the last page
+            if !viewModel.isReachLastPage {
+                self.viewModel.fetchPopular(isFromScroll: true)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let movie = viewModel.dataModel(for: indexPath.row)
+        
+        guard let movie = movie,
+              let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.id) else {
+            return UITableViewCell()
+        }
+        
+        switch cell {
+            
+        case let (cell as MovieTableViewCell):
+            
+            cell.setContent(movie: movie)
+        default:
+            break
+        }
+        
+        return cell
     }
 }
--- MovieRama/MovieRama/UI/PopularMovies/PopularMoviesViewModel.swift
+++ MovieRama/MovieRama/UI/PopularMovies/PopularMoviesViewModel.swift
@@ -4,17 +4,47 @@
 //
 
 import Foundation
+import Combine
 
-protocol PopularMoviesViewModelProtocol {
-    
-    func fetchPopular()
-}
+//protocol PopularMoviesViewModelProtocol {
+//
+//    var reloadData: AnyPublisher<Void, Never> { get }
+//    var isReachLastPage: Bool { get }
+//
+//    func fetchPopular(isFromRefresh: Bool, isFromScroll: Bool)
+//
+//    func dataModel(for index: Int) -> MovieDataModel?
+//    func numberOfRows() -> Int
+//    func refreshContent()
+//}
 
-class PopularMoviesViewModel: PopularMoviesViewModelProtocol {
+class PopularMoviesViewModel {
     
     private var cancellable = Cancellable()
     
-    var service: PopularMoviesServiceProtocol!
+    // MARK: Service
+    
+    private var service: PopularMoviesServiceProtocol!
+    
+    // MARK: ModieDataModel Source
+    
+    private var cachedMovies: [MovieDataModel] = []
+    private var moviesDataModel: [MovieDataModel] = []
+    
+    // MARK: Pagination
+    
+    var pagination: Pagination?
+    private var isLoading: Bool = false
+    
+    // MARK: Reload data Publisher
+    
+    var reloadData: AnyPublisher<Void, Never> {
+        return reloadDataSubject.eraseToAnyPublisher()
+    }
+    
+    private var reloadDataSubject = PassthroughSubject<Void, Never>()
+    
+    // MARK: Init
     
     init(service: PopularMoviesServiceProtocol = PopularMoviesService()) {
         self.service = service
@@ -24,14 +54,85 @@ class PopularMoviesViewModel: PopularMoviesViewModelProtocol {
         print("♻️ PopularMoviesViewModel")
     }
     
-    func fetchPopular() {
+    func fetchPopular(isFromRefresh: Bool = false, isFromScroll: Bool = false) {
         
-        service.fetchPopular(nextPage: 1).done { response in
+        if isLoading { return }
+        let nextPage = isFromScroll ? (pagination == nil ? 1 : pagination!.nextPage) : 1
+        //print("next page: \(nextPage)")
+        isLoading = true
+        
+        service.fetchPopular(nextPage: nextPage).done { [weak self] response in
+            guard let self = self else { return }
             print(response.toPrintedJSON)
+            
+            let pagination = Pagination(currentPage: response.page, nextPage: response.page, totalPages: response.totalPages)
+            self.updatePagination(page: pagination)
+            
+            if isFromRefresh {
+                self.cachedMovies.removeAll()
+            }
+            
+            self.buldDataModel(response.popularMovies)
+            
+            self.isLoading = false
             
         } catchError: { error in
             print(error)
+            self.isLoading = false
             
         }.store(in: &cancellable)
+    }
+    
+    // MARK: Update Pagination
+    
+    private func updatePagination(page: Pagination) {
+        self.pagination = Pagination.update(page)
+        print(pagination!)
+    }
+    
+    // MARK: Refresh Content
+    
+    func refreshContent() {
+        pagination = nil
+        fetchPopular(isFromRefresh: true)
+    }
+    
+    // MARK: Build Movies Data Model
+    
+    private func buldDataModel(_ response: [APIResponsePopularMovie]) {
+        
+        let model = response.compactMap { item -> MovieDataModel in
+            
+            let id = String(item.id)
+            
+            return MovieDataModel(id: id,
+                                  title: item.title,
+                                  voteAveragePercent: item.voteAveragePercent,
+                                  voteAverageValue: item.voteAverage,
+                                  releaseDateFormatted: item.releaseDateFormatted,
+                                  posterImageUrl: item.posterImageUrl)
+        }
+        
+        cachedMovies.append(contentsOf: model)
+        moviesDataModel = cachedMovies
+        reloadDataSubject.send()
+    }
+}
+
+// MARK: DataSource Methods
+
+extension PopularMoviesViewModel {
+    
+    func dataModel(for index: Int) -> MovieDataModel? {
+        guard moviesDataModel.indices.contains(index) else { return nil }
+        return moviesDataModel[index]
+    }
+    
+    func numberOfRows() -> Int {
+        return moviesDataModel.count
+    }
+    
+    var isReachLastPage: Bool {
+        return self.pagination?.reachLastPage ?? false
     }
 }
